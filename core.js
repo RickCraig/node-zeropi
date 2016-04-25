cf5e6c4b8bd176def8aa77f961998d022e4ad152
Rick <rick@spacerevolver.com>
2016-04-25 20:48:20 +0100

    Add initial structure

---
+++ core.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var SerialPort = require('serialport').SerialPort;
+
+class ZeroPiCore {
+  constructor(options) {
+    // Callback placeholders
+    this.onOpen = () => {};
+
+    // Serial port setup
+    this.options = options || { debug: false };
+    this.isOpen = false;
+    this.serialPort = new SerialPort('/dev/ttyAMA0', { baudrate: 115200 });
+    this.serialPort.on('open', this.onOpen);
+  }
+
+  open() {
+    this.isOpen = true;
+    this.onOpen();
+    this.log('Serial port opened successfully');
+    this.serialPort.on('data', this.data);
+  }
+
+  data(data) {
+    let buffer = String(data);
+
+    if (buffer.indexOf('\n') > -1) {
+      buffer = buffer.replace(/\\n|\\r/g, '');
+
+      if (buffer.length > 3 && buffer.indexOf('OK') > -1) {
+        this.getCallback(buffer);
+      }
+    }
+  }
+
+  getCallback(buffer) {
+    if (buffer.indexOf('L') > -1) {
+      const split = buffer.split(' L');
+
+      if (split[0]) {
+        const result = Number(split[1].split(' OK')[0]);
+        this.selectors[split[0]](result);
+      }
+    } else {
+      const name = String(buffer.split(' OK')[0]);
+      if (this.selectors[name]) selectors[name]();
+    }
+  }
+
+  log(message) {
+    if (!this.options.debug) return;
+    console.log(message);
+  }
+
+  onResult(err, results) {
+    if (err) return console.log('An error has occurred during writing', err);
+    this.log(results);
+  }
+
+  write(str) {
+    if (this.isOpen) {
+      this.serialPort.write(`\n${str}\n`, this.onResult);
+    }
+  }
+}
+
+module.exports = ZeroPiCore;
---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var ZeroPiCore = require('./core');
+
+class ZeroPi extends ZeroPiCore {
+  constructor(options) {
+    super(options);
+  }
+
+  digitalWrite(pin, level) {
+    this.write(`M11 D${pin} P${level}`);
+  }
+
+  pwmWrite(pin, pwm) {
+    this.write(`M11 D${pin} P${pwm}`);
+  }
+
+  digitalRead(pin, callback) {
+    this.selectors[`R12 D${pin}`] = callback;
+    this.write(`M12 D${pin}`);
+  }
+
+  analogRead(pin, callback) {
+    this.selectors[`R13 A${pin}`] = callback;
+    this.write(`M13 A${pin}`);
+  }
+
+  dcMotorRun(device, speed) {
+    this.write(`M21 D${device} P${speed}`);
+  }
+
+  dcMotorStop(device) {
+    this.write(`M22 D${device}`);
+  }
+
+  servoRun(device, angle) {
+    this.write(`M41 D${device} A${angle}`);
+  }
+
+  stepperRun(device, speed) {
+    this.write(`M51 D${device} F${speed}`);
+  }
+
+  stepperMove(device, distance, speed, callback) {
+    this.selectors[`R52 D${device}`] = callback;
+    this.write(`M52 D${device} R${distance} F${speed}`);
+  }
+
+  stepperMoveTo(device, position, speed, callback) {
+    this.selectors[`R52 D${device}`] = callback;
+    this.write(`M52 D${device} A${position} F${speed}`);
+  }
+
+  stepperStop(device) {
+    this.write(`M54 D${device}`);
+  }
+
+  steppersEnable() {
+    this.write('M56');
+  }
+
+  steppersDisable() {
+    this.write('M57');
+  }
+
+  stepperSetting(device, microstep, acceleration) {
+    this.write(`M53 D${device} S${microstep} A${acceleration}`);
+  }
+}
+
+module.exports = ZeroPi;
